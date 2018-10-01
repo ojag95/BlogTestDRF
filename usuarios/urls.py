14f37944c6fd521481f9aa92479ac5dcf9bbb2f4
ojag95 <ojag95@gmail.com>
2018-09-30 19:24:05 -0500

    Mejora del DRF permite la solicitud de datos directamente en formato json

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from usuarios import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^usuarios/$', views.lista_usuarios),
     url(r'^usuarios/(?P<pk>[0-9]+)/$', views.detalle_usuarios),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- usuarios/views.py
+++ usuarios/views.py
@@ -5,49 +5,52 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 from usuarios.models import Usuarios
 from usuarios.serializers import UsuariosSerializer
 
-@csrf_exempt
-def lista_usuarios(request):
+@api_view(['GET', 'POST'])
+def lista_usuarios(request, format=None):
     """
     Lista todos los usuarios
     """
     if request.method == 'GET':
         usuarios = Usuarios.objects.all()
         serializador = UsuariosSerializer(usuarios, many=True)
-        return JsonResponse(serializador.data, safe=False)
+        return  Response(serializador.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializador = UsuariosSerializer(data=data)
+        serializador = UsuariosSerializer(data=request.data)
         if serializador.is_valid():
             serializador.save()
-            return JsonResponse(serializador.data, status=201)
-        return JsonResponse(serializador.errors, status=400)
+            return Response(serializador.data, status=status.HTTP_201_CREATED)
+        return Response(serializador.errors, status=status.HTTP_400_BAD_REQUEST)  
     
-@csrf_exempt
-def detalle_usuarios(request, pk):
+      
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalle_usuarios(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
     """
+    
     try:
         usuario = Usuarios.objects.get(pk=pk)
     except Usuarios.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializador = UsuariosSerializer(usuario)
-        return JsonResponse(serializador.data)
+        return Response(serializador.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializador = UsuariosSerializer(usuario, data=data)
+        serializador = UsuariosSerializer(usuario, data=request.data)
         if serializador.is_valid():
             serializador.save()
-            return JsonResponse(serializador.data)
-        return JsonResponse(serializador.errors, status=400)
+            return Response(serializador.data)
+        return Response(serializador.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         usuario.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
